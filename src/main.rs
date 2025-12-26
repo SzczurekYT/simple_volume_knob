bea7d71c190aadcb6dd4fc0d5ada26a342e9c3d0
SzczurekYT <szczurek@szczurek.yt>
2025-12-26 16:54:02 +0100

    feat: Bluetooth is up

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 #![no_std]
 #![no_main]
 
+pub mod bluetooth;
+
 use async_debounce::Debouncer;
+use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_futures::select::select;
 use embassy_rp::{
-    Peri,
-    gpio::{AnyPin, Input, Pull},
+    Peri, bind_interrupts,
+    gpio::{AnyPin, Input, Level, Output, Pull},
+    peripherals::{DMA_CH0, PIO0},
+    pio::{InterruptHandler, Pio},
 };
 use embassy_time::Duration;
 use embedded_hal::digital::InputPin;
 use embedded_hal_async::digital::Wait;
+use static_cell::StaticCell;
+use trouble_host::prelude::ExternalController;
 
 use {defmt_rtt as _, panic_probe as _};
 
@@ -30,13 +37,53 @@ const RIGHT_P2: u8 = 0b1001;
 
 const DEBOUNCE_MS: u64 = 1;
 
+const CYW43_FW: &[u8] = include_bytes!("../cyw43-firmware/43439A0.bin");
+const CYW43_CLM: &[u8] = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
+const CYW43_BTFW: &[u8] = include_bytes!("../cyw43-firmware/43439A0_btfw.bin");
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        cyw43_pio::DEFAULT_CLOCK_DIVIDER,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
+
+    static CYW43_STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let cyw43_state = CYW43_STATE.init(cyw43::State::new());
+    let (_net_device, bt_device, mut control, runner) =
+        cyw43::new_with_bluetooth(cyw43_state, pwr, spi, CYW43_FW, CYW43_BTFW).await;
+    spawner.spawn(cyw43_task(runner)).unwrap();
+    control.init(CYW43_CLM).await;
+
+    let bt_controller: ExternalController<_, 10> = ExternalController::new(bt_device);
+
+    bluetooth::run_bluetooth(bt_controller).await;
+
     spawner
         .spawn(knob_controller(p.PIN_16.into(), p.PIN_17.into()))
         .unwrap();
+}
+
+#[embassy_executor::task]
+async fn cyw43_task(
+    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+) -> ! {
+    runner.run().await
 }
 
 #[embassy_executor::task]
